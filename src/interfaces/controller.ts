bbc1fc4d6b2357e3cf0c7c1b497cc4981f399ea4
Wiliane Souza <wiliane_souza@hotmail.com>
2022-10-18 21:20:21 -0300

    remove interface RequestData

--- src/interfaces/controller.ts
+++ src/interfaces/controller.ts
@@ -1,7 +1,5 @@
-import { RequestData } from "./request-data";
-
 export interface ControllerI {
-  request: (serviceName: string, data: RequestData) => Promise<Object | string>;
+  request: (serviceName: string, data: Object) => Promise<Object | string>;
   register: (serviceName: string) => Promise<Object | string>;
   remove: () => Promise<Object | string>;
 }
